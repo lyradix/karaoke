e29a170de72020adb1349c8983536023c41fb0b3
Daryl St Paul <daryl.stpaul@gmail.com>
2026-02-21 17:51:30 +0100

    client song directory

---
+++ src/app/qr-code/qr-code.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-code',
+  imports: [],
+  templateUrl: './qr-code.html',
+  styleUrl: './qr-code.scss',
+})
+export class QrCode {
+
+}
